8548ed9c624adf6ee2bdc3c079ca784a9ff55094
puli1505 <130792287+puli1505@users.noreply.github.com>
2023-11-17 00:37:47 -0400

    Resultados.cs
    Logica botones 1-10 mostrar preguntas y respuestas

--- Resultados.cs
+++ Resultados.cs
@@ -17,6 +17,55 @@ namespace SistemaEvaluacion
             InitializeComponent();
         }
 
+         private void MostrarPreguntaYRespuestas(int preguntaActualIndex)
+        {
+            // Ruta del archivo CSV
+            string rutaArchivo = "resultados.csv";
+
+            // Verifica si el archivo CSV existe
+            if (File.Exists(rutaArchivo))
+            {
+                // Utiliza TextFieldParser para leer el archivo CSV
+                using (TextFieldParser parser = new TextFieldParser(rutaArchivo))
+                {
+                    parser.TextFieldType = FieldType.Delimited;
+                    parser.SetDelimiters(",");
+
+                    // Salta la primera línea (encabezado)
+                    parser.ReadLine();
+
+                    // Salta las líneas hasta llegar a la pregunta deseada
+                    for (int i = 0; i < preguntaActualIndex; i++)
+                    {
+                        parser.ReadLine();
+                    }
+
+                    // Lee la línea actual como un array de campos
+                    string[] campos = parser.ReadFields();
+
+                    // Crea una nueva instancia de Pregunta con los campos obtenidos
+                    Pregunta preguntaActual = new Pregunta
+                    {
+                        PreguntaTexto = campos[0],
+                        RespuestaCorrecta = campos[1],
+                        RespuestaSeleccionada = campos[2], // Ajusta según tu estructura real
+                        RespuestasBarajeadas = new List<string> { campos[3], campos[4], campos[5], campos[6] }
+                    };
+
+                    // Asigna los valores a las labels
+                    lblPregunta.Text = $"{preguntaActualIndex+1}. {preguntaActual.PreguntaTexto}";
+                    lblRespuestaA.Text = preguntaActual.RespuestasBarajeadas[0];
+                    lblRespuestaB.Text = preguntaActual.RespuestasBarajeadas[1];
+                    lblRespuestaC.Text = preguntaActual.RespuestasBarajeadas[2];
+                    lblRespuestaD.Text = preguntaActual.RespuestasBarajeadas[3];
+                }
+            }
+            else
+            {
+                MessageBox.Show("El archivo CSV no existe.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
         private void pboxPreguntas110_Click(object sender, EventArgs e)
         {
             pnlPreguntas110.Visible = true;
@@ -45,54 +94,54 @@ namespace SistemaEvaluacion
             pnlPreguntas2130.Visible = false;
         }
 
-        private void pboxCasilla1_Click(object sender, EventArgs e)
+                private void pboxCasilla1_Click_1(object sender, EventArgs e)
         {
-
+            MostrarPreguntaYRespuestas(0);
         }
 
         private void pboxCasilla2_Click(object sender, EventArgs e)
         {
-
+            MostrarPreguntaYRespuestas(1);
         }
 
         private void pboxCasilla3_Click(object sender, EventArgs e)
         {
-
+            MostrarPreguntaYRespuestas(2);
         }
 
         private void pboxCasilla4_Click(object sender, EventArgs e)
         {
-
+            MostrarPreguntaYRespuestas(3);
         }
 
         private void pboxCasilla5_Click(object sender, EventArgs e)
         {
-
+            MostrarPreguntaYRespuestas(4);
         }
 
         private void pboxCasilla6_Click(object sender, EventArgs e)
         {
-
+            MostrarPreguntaYRespuestas(5);
         }
 
         private void pboxCasilla7_Click(object sender, EventArgs e)
         {
-
+            MostrarPreguntaYRespuestas(6);
         }
 
         private void pboxCasilla8_Click(object sender, EventArgs e)
         {
-
+            MostrarPreguntaYRespuestas(7);
         }
 
         private void pboxCasilla9_Click(object sender, EventArgs e)
         {
-
+            MostrarPreguntaYRespuestas(8);
         }
 
         private void pboxCasilla10_Click(object sender, EventArgs e)
         {
-
+            MostrarPreguntaYRespuestas(9);
         }
 
         private void pboxCasilla11_Click(object sender, EventArgs e)
